b06227b247aabe62e724a5375ad5823cbb398107
Stefan <stef.lazar2000@gmail.com>
2021-12-09 23:24:43 +0200

    hmm

--- source.cpp
+++ source.cpp
@@ -3,14 +3,32 @@
 //       - Scoaterea in evidenta a zonelor de interes si a punctelor de interes din cadrul acestor zone
 //       - Clasificarea intr-una din cele 7 emotii, in functie de pozitionarea punctelor de interes fata de un anumit 'standard' de normalitate
 
-#include "opencv2/core.hpp"
-#include "opencv2/face.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
-using namespace std;
+#include<iostream>
+
 using namespace cv;
+using namespace std;
 
-using namespace cv::face;
+void main()
+{
+	string path = "Resources/KA.AN1.39.tiff";
+	Mat img = imread(path);
 
+	CascadeClassifier face;
+	face.load("Resources/haarcascade_frontalface.xml");
 
+	vector<Rect> faces;
+	face.detectMultiScale(img, faces, 1.1, 10);
+
+	for (int i = 0; i < faces.size(); i++)
+	{
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 255, 0), 2);
+	}
+	cout << faces.size();
+	imshow("Subject", img);
+	waitKey(0);
+}
